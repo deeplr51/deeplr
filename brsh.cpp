2b125aa8975e4a555ec3769f33919d5394051cd4
deeplr51 <149945974+deeplr51@users.noreply.github.com>
2023-11-05 20:04:41 +0530

    Add files via upload

--- brsh.cpp
+++ brsh.cpp
@@ -1,5 +1,33 @@
 #include<windows.h>
+#include<iostream>
 #include <GL/glut.h>
+#include <cmath>
+void drawVerticalLine(int x, int y1, int y2) {
+  // Calculate the error term.
+  int error = 0;
+
+  // Start at the bottom of the line.
+  int y = y1;
+
+  // Loop until we reach the top of the line.
+  while (y <= y2) {
+    // Draw the pixel at the current position.
+    glBegin(GL_POINTS);
+    glVertex2i(x, y);
+    glEnd();
+
+    // Increment the error term.
+    error += 1;
+
+    // If the error term is greater than or equal to half of the y-step,
+    // increment the y-coordinate.
+    if (error >= (y2 - y1) / 2) {
+      y++;
+      error -= (y2 - y1);
+    }
+  }
+}
+
 
 void drawLine(int x1, int y1, int x2, int y2) {
     int dx = x2 - x1;
@@ -28,17 +56,26 @@ void drawLine(int x1, int y1, int x2, int y2) {
     glEnd();
 }
 
+void drawRightAngleTriangle() {
+    int x1 = 100;
+    int y1 = 100;
+    int x2 = 100;
+    int y2 = 300;
+    int x3 = 300;
+    int y3 = 100;
+
+    // Draw the three sides of the triangle
+    drawVerticalLine(x1, y1, y2);
+    drawLine(x2, y2, x3, y3);
+    drawLine(x3, y3, x1, y1);
+}
+
 void display() {
     glClear(GL_COLOR_BUFFER_BIT);
     glColor3f(1.0, 1.0, 1.0); // Set color to white
     glPointSize(2.0);
 
-    int x1 = 100;
-    int y1 = 100;
-    int x2 = 300;
-    int y2 = 300;
-
-    drawLine(x1, y1, x2, y2);
+    drawRightAngleTriangle();
 
     glFlush();
 }
@@ -47,7 +84,7 @@ int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(400, 400);
-    glutCreateWindow("Bresenham's Line Drawing Algorithm");
+    glutCreateWindow("Right-Angled Triangle with Bresenham's Lines");
     glClearColor(0.0, 0.0, 0.0, 0.0);
     gluOrtho2D(0, 400, 0, 400);
     glutDisplayFunc(display);
